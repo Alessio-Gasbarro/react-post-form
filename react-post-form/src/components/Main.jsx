18fbb9e25a78c73e8692bef49f2474f5dc4ec271
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-14 15:05:35 +0200

    [UPD] Added header, main, footer start base

--- react-post-form/src/components/Main.jsx
+++ react-post-form/src/components/Main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
-const Main = () => {
+const Main = ({ formData, handleChange, handleSubmit }) => {
     return (
-        <div>
+        <>
+            <form action="">
 
-        </div>
+                {/*AUTORE E TITOLO*/}
+                <div>
+                    <label htmlFor="author">Autore</label>
+                    <input type="text" id="author" name="author" value={formData.author} onChange={handleChange} />
+
+                    <label htmlFor="title">title</label>
+                    <input type="title" id="title" name="title" value={formData.title} onChange={handleChange} />
+                </div>
+
+                {/*BODY*/}
+                <div>
+                    <label htmlFor="body">Body</label>
+                    <textarea id="body" name="body" value={formData.body} onChange={handleChange}>
+                    </textarea>
+                </div>
+
+                {/*TEST*/}
+                <div>
+                    <label htmlFor="public">public</label>
+                    <input type="checkbox" id="public" name="public" checked={formData.public} onChange={handleChange}></input>
+                </div>
+
+                {/*INVIA*/}
+                <button type="submit" onClick={handleSubmit}>Invia</button>
+            </form>
+        </>
     )
 }
 
